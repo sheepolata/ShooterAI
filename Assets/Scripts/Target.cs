f1fc9da099cb239ac6aaae75ebebba67343ff94e
Antoine Gaget <antoine.gaget@gmail.com>
2022-03-11 20:12:26 +0100

    Add UI and UX to targets

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -9,8 +9,19 @@ public class Target : MonoBehaviour
     float damageTaken = 0;
 
     Text txt;
-    float resetTimerMax = 2f;
+    public float resetTimerMax = 2f;
     float resetTimer = 2f;
+
+    // float textScale = 0.01f;
+    // float maxTextScale = 0.07f;
+    // float retractSpeed = 0.01f;
+    // float growthTick = 0.005f;
+
+    int textFontSize = 14;
+    int maxTextFontSize = 65;
+    float retractSpeedFontSize;
+    int growthTickFontSize = 4;
+    float fontSizef = 14f;
 
     void Start(){
         transform.GetChild(0).GetComponent<Canvas>().worldCamera = cam;
@@ -26,12 +37,22 @@ public class Target : MonoBehaviour
             txt.text = string.Format("{0:d2}", Mathf.RoundToInt(damageTaken));
             resetTimer = 0;
         }
+
+        // txt.transform.localScale = Vector3.Max(txt.transform.localScale - (Vector3.one * retractSpeed * Time.deltaTime), Vector3.one * textScale);
+        fontSizef = Mathf.Max(fontSizef - (retractSpeedFontSize * Time.deltaTime), textFontSize);
+        txt.fontSize = Mathf.RoundToInt(fontSizef);
     }
 
     public void onShotTaken(float dmg){
         damageTaken += dmg;
         resetTimer = resetTimerMax;
         txt.text = string.Format("{0:d2}", Mathf.RoundToInt(damageTaken));
+
+        // txt.transform.localScale = Vector3.Min(txt.transform.localScale + (Vector3.one * growthTick), new Vector3 (maxTextScale, maxTextScale, maxTextScale));
+        // retractSpeed = (txt.transform.localScale.x - textScale) / resetTimerMax;
+        fontSizef = Mathf.Min(fontSizef + ((float)growthTickFontSize * dmg), (float)maxTextFontSize);
+        retractSpeedFontSize = (fontSizef - textFontSize) / resetTimerMax;
+
     }
 
 
